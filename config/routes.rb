e9aa654dc5921c3f04d2f3d720c6936b8c0dab8f
Nori <kenso1107@outlook.jp>
2020-06-24 05:26:19 +0000

    ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'users/show'
   get 'users/new'
   get 'users/create'
@@ -6,6 +9,10 @@ Rails.application.routes.draw do
   get 'tasks/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
